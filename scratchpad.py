eb0e300a250a5da2d358cf04584b1d4e0d3d329d
YourAverageMo <Yilmaz@arizona.edu>
2024-09-17 14:09:02 -0700

    add download_video()

--- scratchpad.py
+++ scratchpad.py
@@ -1,6 +1,8 @@
 from youtube_to_davinci_resolve import *
 
 # auto-editor "https://www.youtube.com/watch?v=oaiQ5hYKHTE" --margin 0s -o "output" --extras "-c:a libmp3lame -b:a 320k"
+
+# yt-dlp --remux-video mp4 --format "bv+ba[ext=m4a]/ba[ext=aac]" "https://www.youtube.com/watch?v=xxfl7NCzBAA&pp=ygUIc2Z4IG1lbWU%3D"
 '''
 yt-dlp commands:
 
@@ -10,7 +12,7 @@ yt-dlp commands:
 '''
 
 
-def download_video(url: str) -> Path:
+def download_video(url: str, video_title: str) -> Path:
     # set/make temp dir for download
     temp_dir = Path(tempfile.gettempdir(), 'youtube_to_davinci_resolve')
     try:
@@ -21,9 +23,26 @@ def download_video(url: str) -> Path:
         )
         exit()
 
-    # TODO download video
+    result = subprocess.run([
+        'yt-dlp',
+        '--format',
+        'bv+ba[ext=m4a]/ba[ext=aac]',  # best video + m4a or aac
+        '--remux-video',
+        'mp4',
+        '-P',
+        temp_dir,
+        '--output',
+        f"{video_title}.%(ext)s",
+        url,
+    ])
+
+    if result.returncode == 0:
+        # Find the downloaded file in the temp_dir
+        file = list(temp_dir.glob(f"{video_title}*"))[0]
+        return file
+    else:
+        return False
 
 
-
-
-download_video(TEST_LINK)
+file = download_video(TEST_LINK, sanitize_filename(get_video_title(TEST_LINK)))
+print(file)
--- youtube_to_davinci_resolve.py
+++ youtube_to_davinci_resolve.py
@@ -4,11 +4,12 @@ import tempfile
 from pathlib import Path
 import yt_dlp
 
+# TODO emojis in video_title may cause issues. keep an eye on it
 # TODO move global vars to a json file
 # TODO before that convert global vars to dict so its easier to incorporate json later
 UNWANTED_WORDS = [
     'Sound', 'effect', 'for editing', 'editing', '(dl in desc)', '-', '()',
-    "''"
+    "''", '.'
 ]
 SFX_KEYWORDS = [
     'sfx',
@@ -18,6 +19,12 @@ SFX_KEYWORDS = [
 # yes im using an asmongold clip... bite me
 TEST_LINK = "https://www.youtube.com/watch?v=Xgf8UBxKii0"
 AUTO_DELETE_TEMP = False
+
+
+def get_clipboard() -> str:
+    """gets last item in users clipboard."""
+    link = subprocess.getoutput("powershell.exe -Command Get-Clipboard")
+    return link
 
 
 def get_video_title(url: str) -> str:
@@ -52,13 +59,43 @@ def sanitize_filename(filename: str) -> str:
     filename = ' '.join(filename.split())
     filename = filename.title()
 
+    # max filename length
+    if len(filename) > 99:
+        filename = filename[:99]
+
     if not filename:
         filename = 'Untitled'
 
     return filename
 
 
-def get_clipboard() -> str:
-    """gets last item in users clipboard."""
-    link = subprocess.getoutput("powershell.exe -Command Get-Clipboard")
-    return link
+def download_video(url: str, video_title: str) -> Path:
+    # set/make temp dir for download
+    temp_dir = Path(tempfile.gettempdir(), 'youtube_to_davinci_resolve')
+    try:
+        temp_dir.mkdir(exist_ok=True)
+    except FileNotFoundError:
+        print(
+            f"Your temp folder ({temp_dir.parent}) was not found. For caution, the script will not create it. please double check your temp dir and try again.\nExiting script..."
+        )
+        exit()
+
+    result = subprocess.run([
+        'yt-dlp',
+        '--format',
+        'bv+ba[ext=m4a]/ba[ext=aac]',  # best video + m4a or aac
+        '--remux-video',
+        'mp4',
+        '-P',
+        temp_dir,
+        '--output',
+        f"{video_title}.%(ext)s",
+        url,
+    ])
+
+    if result.returncode == 0:
+        # Find the downloaded file in the temp_dir
+        file = list(temp_dir.glob(f"{video_title}*"))[0]
+        return file
+    else:
+        return False
